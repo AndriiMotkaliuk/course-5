d1dc0e33d8c9542c16643075780b90428863c02f
Andrii Motkaliuk <motkaliuk.work@gmail.com>
2023-11-29 00:01:15 +0200

    images added

--- students/motkaliuk_andrii/weather_exam_3/src/App.js
+++ students/motkaliuk_andrii/weather_exam_3/src/App.js
@@ -19,9 +19,12 @@ import thunderstorm_image from './images/11_thunderstorm.jpg';
 import snow_image from './images/13_snow.jpg';
 import mist_image from './images/50_mist.jpg';
 
-
-
-
+import clear_sky_night_image from './images/01_clear_sky_night.jpg';
+import few_clouds_night_image from './images/02_few_clouds_night.jpg';
+import scattered_clouds_night_image from './images/03_scattered_clouds_night.jpg';
+import broken_clouds_night_image from './images/04_broken_clouds_night.jpg';
+import shower_rain_night_image from './images/09_shower_rain_night.jpg';
+import rain_night_image from './images/10_rain_night.jpg';
 
 function App() {
 
@@ -47,48 +50,47 @@ function App() {
     fetchWeather();
   }, [query, units]);
 
-  // const getBackgraundImage = () => {
-  //   console.log(weather)
-  // }
-
   const getBackgroundImage = () => {
     if (!weather) return
-    // console.log(weather.icon)
 
     switch (weather.icon) {
-      case '01n':
       case '01d':
         return clear_sky_image;
+      case '01n':
+        return clear_sky_night_image;
       case '02d':
         return few_clouds_image;
-      case '03n':
+      case '02n':
+        return few_clouds_night_image;
+      case '03d':
         return scattered_clouds_image;
-      case '04n':
+      case '03n':
+        return scattered_clouds_night_image;
       case '04d':
         return broken_clouds_image;
+      case '04n':
+        return broken_clouds_night_image;
       case '09d':
         return shower_rain_image;
+      case '09n':
+        return shower_rain_night_image;
       case '10d':
         return rain_image;
+      case '10n':
+        return rain_night_image;
       case '11d':
+      case '11n':
         return thunderstorm_image;
       case '13d':
+      case '13n':
         return snow_image;
       case '50d':
+      case '50n':
         return mist_image;
       default:
         return few_clouds_image;
     }
   };
-
-  // const formatBackground = () => {
-
-  //   const iconCode = weather.weather[0]?.icon;
-  //   const backgroundUrl = getBackgroundImage(iconCode);
-
-  //   return `url(${backgroundUrl})`;
-  // };
-
 
   return (
 
@@ -114,7 +116,7 @@ function App() {
           </div>
         )}
 
-        <ToastContainer autoClose={1000} theme='colored' newestOnTop={true} />
+        <ToastContainer autoClose={700} theme='colored' newestOnTop={true} />
 
       </div>
     </div >
--- students/motkaliuk_andrii/weather_exam_3/src/layout/Home.jsx
+++
@@ -1,7 +0,0 @@
-function Home() {
-    return (<h1 className="text-5xl">Home</h1>);
-}
-
-export { Home as Component };
-
-export default Home;
